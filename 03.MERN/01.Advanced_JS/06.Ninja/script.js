ea924931b369da891870fbc94f208353576163c8
Alvaro Basoalto <alvaro.basoalto@gmail.com>
2023-06-28 19:41:17 -0400

    OOP Ninja Assignment

---
+++ 03.MERN/01.Advanced_JS/06.Ninja/script.js
@@ -0,0 +1,25 @@
+// parent Vehicle class
+class Ninja {
+	constructor(name, health) {
+		this.name = name;
+		this.health = health;
+		this.speed = 3;
+		this.strength = 3;
+	}
+	sayname() {
+		console.log(`Your name is ${this.name}.`);
+	}
+	showstats() {
+		console.log(`This are your stats:
+        Name: ${this.name}.
+        Strenght: ${this.strength}.
+        Health: ${this.health}.`);
+	}
+	drinkSake() {
+		this.health += 10;
+	}
+}
+
+const test = new Ninja('Naya', 100)
+test.sayname();
+test.showstats();
