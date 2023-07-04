de69123a9cb6a219ad13ac65451d44d53f5b5cb3
Alvaro Basoalto <alvaro.basoalto@gmail.com>
2023-07-03 21:14:21 -0400

    First React JSX Assignment

--- 03.MERN/02.React/02.first_react_jsx/src/App.js
+++ 03.MERN/02.React/02.first_react_jsx/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				<h1>Hello Dojo!</h1>
+				<h2>Things I need to do:</h2>
+				<ul>
+					<li>Learn React</li>
+					<li>Climb Mt Everest</li>
+					<li>Run a marathon</li>
+					<li>Feed the dogs</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
