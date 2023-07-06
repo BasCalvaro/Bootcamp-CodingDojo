63bc8d2736e293d1bb443b1ec8461ae6774b82b5
Alvaro Basoalto <alvaro.basoalto@gmail.com>
2023-07-06 19:31:08 -0400

    Projects start point

---
+++ 03.MERN/02.React/03.support/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+	return (
+		<div className="App">
+			<div className="container">
+				<h1>Hello Dojo!</h1>
+				<h2>Things I need to do:</h2>
+				<ul>
+					<li>Learn React</li>
+					<li>Climb Mt Everest</li>
+					<li>Run a marathon</li>
+					<li>Feed the dogs</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default App;
