90cecde3b8df4b69377c9fc39ba9867988cd31e3
Alvaro Basoalto <alvaro.basoalto@gmail.com>
2023-07-26 00:26:17 -0400

    Nav Bar Context Assignment

---
+++ 03.MERN/02.React/13.nav_bar_context/src/components/Form.js
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
+
+const Form = () => {
+	// ---------------------------------------------
+	// I) VARIABLES & HOOKS
+	// ---------------------------------------------
+
+	const { username, setUsername } = useContext(UserContext);
+
+	// ---------------------------------------------
+	// II) HANDLERS & AUX FUNCTIONS
+	// ---------------------------------------------
+
+	const handleChange = (e) => {
+		setUsername(e.target.value);
+	};
+
+	// ---------------------------------------------
+	// III) JSX
+	// ---------------------------------------------
+
+	return (
+			<div className="d-flex w-50 mb-3">
+				<label for="exampleInputEmail1" className="w-100 form-label">
+					Cambiar nombre de usuario:
+				</label>
+				<input
+					type="text"
+					value={username}
+					onChange={handleChange}
+					className="form-control w-100"
+					placeholder="Ingrese su nombre"
+				/>
+			</div>
+	);
+};
+
+export default Form;
---
+++ 03.MERN/02.React/13.nav_bar_context/src/components/FormWrapper.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Form from "./Form";
+
+const FormWrapper = () => {
+	// ---------------------------------------------
+	// I) VARIABLES & HOOKS
+	// ---------------------------------------------
+
+	// ---------------------------------------------
+	// II) HANDLERS & AUX FUNCTIONS
+	// ---------------------------------------------
+
+	// ---------------------------------------------
+	// III) JSX
+	// ---------------------------------------------
+
+	return (
+		<div className="d-flex justify-content-start p-3">
+			<Form />
+		</div>
+	);
+};
+
+export default FormWrapper;
