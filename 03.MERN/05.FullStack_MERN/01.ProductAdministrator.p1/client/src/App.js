643488fe890889c6028969df3d0c6dd6812e5ce1
Alvaro Basoalto <alvaro.basoalto@gmail.com>
2023-08-02 11:33:12 -0400

    Product Administrator Part I Assignment

---
+++ 03.MERN/05.FullStack_MERN/01.ProductAdministrator.p1/client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+
+import Main from './views/main';
+
+function App() {
+  return (
+    <div>      
+			<h1 className='p-3 text-center'>Product Manager</h1>
+      <Main />
+    </div>
+  );
+}
+export default App;
---
+++ 03.MERN/05.FullStack_MERN/01.ProductAdministrator.p1/client/src/views/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import PersonForm from "../components/productForm";
+
+function Main () {
+	return (
+		<div className="d-flex justify-content-center p-3">
+			<PersonForm />
+		</div>
+	);
+};
+
+export default Main;
